c95917aebe42f090df761b5d7e0cf6565b97c679
zxzinder <zinder@alexzinerdemini.lan>
2016-03-22 23:11:35 +0800

    home model

---
+++ BTApp/Others/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BTApp
+//
+//  Created by MacMini on 16/3/22.
+//  Copyright © 2016年 MacMini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
