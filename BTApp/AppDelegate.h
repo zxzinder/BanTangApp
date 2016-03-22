f7bf751dcb87b507cdbc0a6fc8f87f26ad95fb0b
zx <zxzinder@163.com>
2016-03-22 10:24:42 +0800

    Initial Commit

---
+++ BTApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BTApp
+//
+//  Created by MacMini on 16/3/22.
+//  Copyright © 2016年 MacMini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
